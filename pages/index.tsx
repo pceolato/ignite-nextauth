1487c35e84ff1e7cf731b7f158f2807bc3e5c480
Pedro Ceolato <pedroceolato2002@gmail.com>
2022-07-14 20:46:33 -0300

    feat: salvando dados do usuário

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const { signIn } = useContext(AuthContext)
+  const { signIn } = useContext(AuthContext);
 
   async function handleSubmit(event: FormEvent) {
     event.preventDefault();
