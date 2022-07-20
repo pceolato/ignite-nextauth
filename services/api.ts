c36bc08bb08c2cb31f93298f0a9e3bbd66b23051
Pedro Ceolato <pedroceolato2002@gmail.com>
2022-07-20 20:05:48 -0300

    feat: logout automático

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError } from 'axios';
 import { parseCookies, setCookie } from 'nookies';
+import { signOut } from '../contexts/AuthContext';
 
 let cookies = parseCookies()
 let isRefreshing = false
@@ -66,7 +67,9 @@ api.interceptors.response.use(response => {
                 })
             })
         } else {
-            //deslogar o usuário
+            signOut()
         }
     }
+
+    return Promise.reject(error)
 })
