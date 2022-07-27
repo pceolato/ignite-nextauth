ad3d29deadb0c3b738161fc8b5801f9e2b479127
Pedro Ceolato <pedroceolato2002@gmail.com>
2022-07-26 23:35:49 -0300

    feat: validando permissões

--- utils/withSSRAuth.ts
+++ utils/withSSRAuth.ts
@@ -1,16 +1,44 @@
 import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from "next"
 import { destroyCookie, parseCookies } from "nookies"
 import { AuthTokenError } from "../errors/AuthTokenError"
+import decode from 'jwt-decode'
+import { validadeUserPermissions } from "./validadeUserPermissions"
 
-export function withSSRAuth<P>(fn: GetServerSideProps<P>): GetServerSideProps {
+type withSSRAuthOptions = {
+    permissions?: string[];
+    roles?: string[];
+}
+
+export function withSSRAuth<P>(fn: GetServerSideProps<P>, options?: withSSRAuthOptions): GetServerSideProps {
     return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<P>> => {
         const cookies = parseCookies(ctx)
+        const token = cookies['nextauth.token']
 
         if (!cookies['nextauth.token']) {
             return {
                 redirect: {
                     destination: '/',
                     permanent: false,
+                }
+            }
+        }
+
+        if (options) {
+            const user = decode<{ permissions: string[], roles: string[] }>(token)
+            const { permissions, roles } = options
+
+            const userHasValidPermissions = validadeUserPermissions({
+                user,
+                permissions,
+                roles
+            })
+
+            if(!userHasValidPermissions) {
+                return {
+                    redirect: {
+                        destination: '/dashboard',
+                        permanent: false
+                    }
                 }
             }
         }
